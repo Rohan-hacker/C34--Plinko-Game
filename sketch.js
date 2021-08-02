b91237ebb8414a8a56252a36cb5d58edded09692
Rohan-hacker <83770475+Rohan-hacker@users.noreply.github.com>
2021-08-02 18:50:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const Engine = Matter.Engine,
-  World = Matter.World,
-  Events = Matter.Events,
-  Bodies = Matter.Bodies;
+const World = Matter.World,
+const Events = Matter.Events,
+const Bodies = Matter.Bodies;
  
 var particles = [];
 var plinkos = [];
